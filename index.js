28295798d6dc1b11f3ce666f583e7f234188b403
Dilusha Sindutharu <sindutharu821@gmail.com>
2024-10-28 22:03:55 +0000

    Member Registration Post method

--- index.js
+++ index.js
@@ -13,13 +13,28 @@ mongoose.connect(con_string).then(() => {
   console.log("DB connected");
 });
 
-const schema = new mongoose.Schema({
+const schema_signup = new mongoose.Schema({
   fullName: String,
   email: String,
   password: String,
 });
 
-const user = mongoose.model("user", schema);
+const schema_memberreg = new mongoose.Schema({
+  firstName: String,
+  middleName: String,
+  sirName: String,
+  dob: String,
+  nameOfMother: String,
+  nameOfFather: String,
+  address: String,
+  birthPlace: String,
+  contactNo: String,
+  marriedDate: String,
+  marridChurch: String,
+});
+
+const user = mongoose.model("user", schema_signup);
+const member = mongoose.model("member_registration", schema_memberreg);
 
 app.get("/", async (req, res) => {
   // res.send("Hello World!");
@@ -40,6 +55,7 @@ app.get("/", async (req, res) => {
   //   .catch((error) => console.log("Error: " + error));
 });
 
+//signup process
 app.post("/signup", (req, res) => {
   const usersList = req.body;
   console.log(usersList);
@@ -61,6 +77,7 @@ app.post("/signup", (req, res) => {
   });
 });
 
+//sign in process
 app.post("/signin", (req, res) => {
   const loginUser = req.body;
   console.log(loginUser);
@@ -83,4 +100,52 @@ app.post("/signin", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+//new member registration
+app.post("/member-registration", (req, res) => {
+  const memberDetails = req.body;
+  console.log(memberDetails);
+
+  const newMember = new member({
+    firstName: memberDetails.firstName,
+    middleName: memberDetails.middleName,
+    sirName: memberDetails.sirName,
+    dob: memberDetails.dob,
+    nameOfMother: memberDetails.nameOfFather,
+    nameOfFather: memberDetails.nameOfFather,
+    address: memberDetails.address,
+    birthPlace: memberDetails.birthPlace,
+    contactNo: memberDetails.contactNo,
+    marriedDate: memberDetails.marriedDate,
+    marridChurch: memberDetails.marridChurch,
+  });
+
+  newMember
+    .save()
+    .then(() => console.log("Member was Added"))
+    .catch((err) => console.log("Error: " + err));
+  // Send a response to client that will show that the request was successfull.
+
+  res.send({
+    message: "A New member was registered.",
+  });
+});
+
+app.patch("/member/:id", async (req, res) => {
+  const {id} = req.params;
+  const updates = req.body;
+
+  try {
+    const updatedMember = await Member.findByIdAndUpdate(id, updates, {
+      new: true,
+    });
+    if (!updatedMember) {
+      return res.status(404).send({message: "Member not found."});
+    }
+    res.send({message: "Member updated successfully.", updatedMember});
+  } catch (err) {
+    console.error("Error updating member: ", err);
+    res.status(500).send("Error updating member");
+  }
 });
